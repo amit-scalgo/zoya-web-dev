4b5fde6f22eff478a7ae3cfe540b2f5be7e48eac
Amit John <amit.p@scalgo.net>
2025-01-24 15:43:31 +0530

    update

--- src/routes/Auth.Routes.jsx
+++ src/routes/Auth.Routes.jsx
@@ -1,13 +1,13 @@
-import Login from "@/pages/auth/Login";
-import Register from "@/pages/auth/Register";
+import Login from '@/pages/auth/Login';
+import Register from '@/pages/auth/Register';
 
 export const AuthRoutes = [
-  {
-    path: "login",
-    element: <Login />,
-  },
-  {
-    path: "register",
-    element: <Register />,
-  },
+    {
+        path: 'login',
+        element: <Login />,
+    },
+    // {
+    //   path: "register",
+    //   element: <Register />,
+    // },
 ];
